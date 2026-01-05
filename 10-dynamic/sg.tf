1690d11d5c43236eda596ecc5aa55a20774385f5
Ram-eng01 <rammohan63025@gmail.com>
2026-01-05 12:03:51 +0530

    terraform-aws

--- 10-dynamic/sg.tf
+++ 10-dynamic/sg.tf
@@ -2,15 +2,26 @@ resource "aws_security_group" "dynamic_sg" {
   name = "dynamic-security-group"
   description = "This is created by using terraform"
 
-  dynamic "ingress" {
-    for_each = toset(var.ports)
-    content {
-      from_port = ingress.value
-      to_port = ingress.value
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+#   dynamic "ingress" {
+#     for_each = toset(var.ports)
+#     content {
+#       from_port = ingress.value
+#       to_port = ingress.value
+#       protocol = "tcp"
+#       cidr_blocks = ["0.0.0.0/0"]
+#     }
+#   }
+
+    dynamic "ingress" {
+        for_each = toset(var.allow_ports)
+        content {
+            from_port = ingress.value["from_port"]
+            to_port = ingress.value["to_port"]
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+      
     }
-  }
 
   egress {
     protocol = "-1"
--- 10-dynamic/variable.tf
+++ 10-dynamic/variable.tf
@@ -1,3 +1,22 @@
-variable "ports" {
-    default = ["22","80","8080","8081","9000","5000"]
+# variable "ports" {
+#     default = ["22","80","8080","8081","9000","5000"]
+# }
+
+
+variable "allow_ports" {
+   default = [
+    {
+        from_port = 22
+        to_port = 22
+    },
+    {
+        from_port = 8080
+        to_port = 8080
+    },
+    {
+        from_port = 80
+        to_port = 80
+    }
+
+   ]
 }
